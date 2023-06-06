0d04626b21f4eb152db54e2a2af126b130622a8f
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2023-06-06 08:39:00 +0300

    gjfjgt

--- MarketPlaceEnd/Pages/MainMainPage.xaml.cs
+++ MarketPlaceEnd/Pages/MainMainPage.xaml.cs
@@ -24,7 +24,7 @@ namespace MarketPlaceEnd.Pages
         {
             InitializeComponent();
             var products = App.db.Product.ToList();
-            MainList.ItemsSource = products;
+            //MainList.ItemsSource = products;
         }
     }
 }
